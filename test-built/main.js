a2cfa2ed94d052a5054dca050e231dc6a31cba7d
Joel Brandt <joelrbrandt@gmail.com>
2014-05-26 21:04:39 -0700

    add jquery

--- test-built/main.js
+++ test-built/main.js
@@ -1,13 +1,18 @@
 require.config({
     paths: {
         "lib" : "../build/lib-built",
+        "jquery" : "../bower_components/jquery/dist/jquery.min"
     }
 });
 
 define(function (require) {
     "use strict";
 
-    var lib = require("lib");
-    lib.addThenMult(2,3,4).then(alert);
+    var lib = require("lib"),
+    	$ = require("jquery");
+
+    lib.addThenMult(2,3,4).then(function (answer) {
+    	$(".answer-text").text(answer);
+    });
 
 });
--- test-unbuilt/main.js
+++ test-unbuilt/main.js
@@ -2,13 +2,18 @@ require.config({
     packages : [{ name: "lib", location: "../src", main: "lib" }],
     paths: {
         "bluebird" : "../bower_components/bluebird/js/browser/bluebird",
+        "jquery" : "../bower_components/jquery/dist/jquery.min"
     }
 });
 
 define(function (require) {
     "use strict";
 
-    var lib = require("lib");
-    lib.addThenMult(2,3,4).then(alert);
+    var lib = require("lib"),
+    	$ = require("jquery");
+
+    lib.addThenMult(2,3,4).then(function (answer) {
+    	$(".answer-text").text(answer);
+    });
 
 });
